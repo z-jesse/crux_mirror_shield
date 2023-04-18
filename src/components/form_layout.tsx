9741be46b8e0a4bc322343323da4ddf1177ba595
jesse1zhang@gmail.com <43320266+giggitymygoo@users.noreply.github.com>
2023-04-17 19:58:44 -0700

    fixup

--- src/components/form_layout.tsx
+++ src/components/form_layout.tsx
@@ -1,7 +1,7 @@
 export default function FormLayout(props: any) {
   return (
-    <div className='grow bg-defgray'>
-      <div className='max-w-4xl mx-auto'>
+    <div className='flex grow bg-defgray'>
+      <div className='flex flex-col w-full max-w-4xl mx-auto justify-center'>
         {props.children}
       </div>
     </div>
--- src/pages/waitlist/index.tsx
+++ src/pages/waitlist/index.tsx
@@ -4,9 +4,9 @@ import WaitlistForm from "@/forms/waitlist_form";
 export default function Waitlist() {
   return (
     <FormLayout>
-      <div className="h-[150px]"></div>
-      <div className='container flex flex-col max-w-4xl md:flex-row mx-auto'>
-        <div className='md:w-1/2'>
+      {/* <div className="h-[100px]"></div> */}
+      <div className='w-full'>
+        {/* <div className='md:w-1/2'>
           <h1 className='uppercase font-condensed font-bold text-6xl text-gold'>
             Waitlist
           </h1>
@@ -16,7 +16,13 @@ export default function Waitlist() {
         </div>
         <div className='md:w-1/2'>
           <WaitlistForm />
-        </div>
+        </div> */}
+        <iframe
+          className="w-full h-[860px]"
+          src="https://docs.google.com/forms/d/e/1FAIpQLScQ9glpkyfBr5amhLjG0k8rKlMnq2Fc5MJOnLgcmo6CNKTbqQ/viewform?embedded=true" 
+          >
+            Loading…
+        </iframe>
       </div>
     </FormLayout>
   )
