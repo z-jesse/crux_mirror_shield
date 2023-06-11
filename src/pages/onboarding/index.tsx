0018768358d235217d89059e299a01f9a0f4f388
jesse1zhang@gmail.com <43320266+z-jesse@users.noreply.github.com>
2023-06-10 17:13:48 -0700

    fix formatting on login signup

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -3,6 +3,7 @@ import Router from 'next/router';
 import { CREATE_USER } from '@/graphql/mutations/onboarding';
 import { GET_ACCOUNT_INFO } from '@/graphql/queries/account';
 import { useMutation, useQuery } from '@apollo/client';
+import FormLayout from '@/components/form_layout';
 
 export default function SignUp() {
     const { loading: queryLoading, error: queryError, data: queryData } = useQuery(GET_ACCOUNT_INFO);
@@ -66,23 +67,24 @@ export default function SignUp() {
     }
 
     return (
+        <FormLayout>
         <form onSubmit={registerUser}>
             <div className='flex flex-wrap'>
-                <div className='w-full md:w-1/2 px-3 mb-3 md:mb-4'>
+                <div className='w-full px-3 mb-3 md:mb-4'>
                     <label className='block font-mono text-white' htmlFor="grid-first-name">Email</label>
                     <input className='block w-full py-2 px-3 bg-gray-200 text-gray-700 font-mono' id="email" type="text" placeholder="example@email.com"/>
                 </div>
             </div>
 
             <div className='flex flex-wrap'>
-                <div className='w-full md:w-1/2 px-3 mb-3 md:mb-4'>
+                <div className='w-full  px-3 mb-3 md:mb-4'>
                     <label className='block font-mono text-white' htmlFor="grid-last-name">Password</label>
                     <input className='block w-full py-2 px-3 bg-gray-200 text-gray-700 font-mono' id="password" type="password" placeholder="**********"/>
                 </div>
             </div>
 
             <div className='flex flex-wrap'>
-                <div className='w-full md:w-1/2 px-3 mb-3 md:mb-4'>
+                <div className='w-full  px-3 mb-3 md:mb-4'>
                     <label className='block font-mono text-white' htmlFor="grid-last-name">Confirm Password</label>
                     <input className='block w-full py-2 px-3 bg-gray-200 text-gray-700 font-mono' id="confirm_password" type="password" placeholder="**********"/>
                 </div>
@@ -92,5 +94,6 @@ export default function SignUp() {
                 <button className='uppercase bg-gold hover:bg-white text-black font-mono py-2 px-20' type='submit'>Sign up</button>
             </div>
         </form>
+        </FormLayout>
     )
 }
--- src/pages/onboarding/login/index.tsx
+++ src/pages/onboarding/login/index.tsx
@@ -3,6 +3,7 @@ import Router from 'next/router';
 import { useMutation, useQuery } from '@apollo/client';
 import { SIGNIN_USER } from '@/graphql/mutations/authentication';
 import { GET_ACCOUNT_INFO } from '@/graphql/queries/account';
+import FormLayout from '@/components/form_layout';
 
 export default function Login() {
     const { loading: queryLoading, error: queryError, data: queryData } = useQuery(GET_ACCOUNT_INFO);
@@ -36,16 +37,17 @@ export default function Login() {
     }
 
     return (
+        <FormLayout>
         <form onSubmit={login}>
             <div className='flex flex-wrap'>
-                <div className='w-full md:w-1/2 px-3 mb-3 md:mb-4'>
+                <div className='w-full  px-3 mb-3 md:mb-4'>
                     <label className='block font-mono text-white' htmlFor="grid-first-name">Email</label>
                     <input className='block w-full py-2 px-3 bg-gray-200 text-gray-700 font-mono' id="email" type="text" placeholder="example@email.com"/>
                 </div>
             </div>
 
             <div className='flex flex-wrap'>
-                <div className='w-full md:w-1/2 px-3 mb-3 md:mb-4'>
+                <div className='w-full  px-3 mb-3 md:mb-4'>
                     <label className='block font-mono text-white' htmlFor="grid-last-name">Password</label>
                     <input className='block w-full py-2 px-3 bg-gray-200 text-gray-700 font-mono' id="password" type="password" placeholder="**********"/>
                 </div>
@@ -55,5 +57,6 @@ export default function Login() {
                 <button className='uppercase bg-gold hover:bg-white text-black font-mono py-2 px-20' type='submit'>Sign up</button>
             </div>
         </form>
+        </FormLayout>
     )
 }
