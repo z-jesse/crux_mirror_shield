67938b3ff21f9e3744e03befb764689090c70bbd
jesse1zhang@gmail.com <43320266+z-jesse@users.noreply.github.com>
2023-08-05 18:18:49 -0700

    fix main color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
       colors: {
         'gold': '#e0c56e',
         'defwhite': '#f5f5f5',
-        'defgray': '#1c1c1c'
+        'defgray': '#1c1c1c',
+        'custom_purple': '#9b80ff',
+        'custom_gold': '#ffe680',
       },
       fontFamily: {
         condensed: ['var(--font-barlow-condensed)'],
